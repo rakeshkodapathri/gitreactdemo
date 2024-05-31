98348b13cfcc2082d0b3827f990bf80aa1da59e8
rakeshkodapathri <rakesh.kodapathri@gmail.com>
2024-05-31 12:41:41 -0400

    second commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
+import Cart from './components/Cart';
 
 function App() {
   return (
